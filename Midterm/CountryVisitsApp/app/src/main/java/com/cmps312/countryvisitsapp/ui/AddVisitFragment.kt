633ea206c71a666898cd7224ec6aedac23589231
AbdullaKhedr <abdolla-tareq@hotmail.com>
2020-10-08 17:11:38 +0300

    adding new visitedCountry implemented

--- Midterm/CountryVisitsApp/app/src/main/java/com/cmps312/countryvisitsapp/ui/AddVisitFragment.kt
+++ Midterm/CountryVisitsApp/app/src/main/java/com/cmps312/countryvisitsapp/ui/AddVisitFragment.kt
@@ -5,7 +5,9 @@ import android.view.View
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.cmps312.countryvisitsapp.R
+import com.cmps312.countryvisitsapp.model.VisitedCountry
 import com.cmps312.countryvisitsapp.repository.Repository
 import kotlinx.android.synthetic.main.fragment_add_visit.*
 
@@ -17,6 +19,7 @@ class AddVisitFragment : Fragment(R.layout.fragment_add_visit) {
         super.onViewCreated(view, savedInstanceState)
 
         val countriesNames = Repository.countries.map { it.name }
+
         val adapter = ArrayAdapter<String>(
             requireContext(),
             android.R.layout.simple_list_item_1, countriesNames
@@ -24,7 +27,19 @@ class AddVisitFragment : Fragment(R.layout.fragment_add_visit) {
         countryNameSpinner.adapter = adapter
 
         addBtn.setOnClickListener {
-            
+
+            val selectedCountryCode =
+                Repository.countries.find { countryNameSpinner.selectedItem.toString() == it.name }
+            val newVisitedCountry = VisitedCountry(
+                selectedCountryCode?.code.toString(),
+                countryNameSpinner.selectedItem.toString(),
+                ratingBar2.rating,
+                amountEdt.text.toString().toFloat()
+            )
+            visitedCountryViewModel.newVisitedCountry = newVisitedCountry
+            visitedCountryViewModel.addCountry()
+
+            findNavController().navigate(R.id.action_addVisitFragment_to_visitsFragment)
         }
 
     }
