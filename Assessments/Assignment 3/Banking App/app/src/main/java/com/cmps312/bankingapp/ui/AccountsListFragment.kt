ec9454803b1dfe79f27ae27adaff90ccbcf9354f
AbdullaKhedr <abdolla-tareq@hotmail.com>
2020-10-25 14:30:41 +0300

    nav_graph and fragments added

---
+++ Assessments/Assignment 3/Banking App/app/src/main/java/com/cmps312/bankingapp/ui/AccountsListFragment.kt
@@ -0,0 +1,12 @@
+package com.cmps312.bankingapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.cmps312.bankingapp.R
+
+class AccountsListFragment : Fragment(R.layout.fragment_accounts_list) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ Assessments/Assignment 3/Banking App/app/src/main/java/com/cmps312/bankingapp/ui/AddAccountFragment.kt
@@ -0,0 +1,12 @@
+package com.cmps312.bankingapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.cmps312.bankingapp.R
+
+class AddAccountFragment : Fragment(R.layout.fragment_add_account) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
